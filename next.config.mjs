5fd2a479e9c06fd0ede7b701a47f6500e0a77371
akshaii7 <akshaistackup@gamil.com>
2026-05-09 16:21:04 +0530

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/stackuptech',
+  assetPrefix: '/stackuptech',
 };
 
 export default nextConfig;
